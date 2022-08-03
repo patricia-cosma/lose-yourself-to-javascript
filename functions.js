c463b55776b08d2c80d5dc9d0c6b5490ffd0343c
patricia.cosma <p.n.cosma@gmail.com>
2022-08-03 13:16:35 +0300

    functions

--- functions.js
+++ functions.js
@@ -30,3 +30,22 @@ letterFinder ('test', 't');
 // --- No match found at 1
 // --- No match found at 2
 // Found the t at 3
+
+// function with defensive programming
+function letterFinder(word, match) {
+    var condition1 = typeof(word) == 'string' && word.length >=2;
+    var condition2 = typeof(match) == 'string' && match.length == 1;
+    if (condition1 == true && condition1 == true) {
+        for(i = 0; i < word.length; i++) {
+            if(word[i] == match) {
+                console.log('Found the', match, 'at', i)
+            } else {
+                console.log('---No match found at', i)
+            }
+        }
+    } else {
+        console.log ('Please pass correct arguments to the function');
+    }
+}
+// letterFinder (2,2); // run this for a failed test
+// letterFinder ('cat', 'c'); // tun this for a passed test
