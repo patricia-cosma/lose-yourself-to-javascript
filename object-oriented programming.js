fb2fd7d2e7ea93176935feeb0cc7431907c77b6d
patricia.cosma <p.n.cosma@gmail.com>
2022-08-10 18:08:51 +0300

    how to design an OOP

--- object-oriented programming.js
+++ object-oriented programming.js
@@ -142,3 +142,133 @@ var boxingGym = new Gym("7-22", "right corner", "left corner")
 console.log(boxingGym.openHrs)
 console.log(boxingGym.stationaryBike) 
 console.log(boxingGym.treadmill) 
+
+/* designing an OOP
+essential keywords: extends and super
+1. thinking of the inheritance hierarchy I want to build 
+2. code the structure of the hierarchy (namely the base class and the sub-class place)
+3. ask the following questions: what should go into the base class? what will all the sub-classes inherit?
+what are the specific properties and methods that separate each class from others? how will the classes relate to one another?
+4. add all the requirements as comments inside the code structure
+5. start coding each class as per the specifications added as comments
+6. after that, build various objects for the sub-classes
+*/
+class Animal {
+    constructor(color = 'yellow', energy = 100) {
+        this.color = color;
+        this.energy = energy;
+    }
+    isActive() {
+        if(this.energy > 0) {
+            this.energy -= 20;
+            console.log('Energy is decreasing, currently at:', this.energy)
+        } else if(this.energy == 0){
+            this.sleep();
+        }
+    }
+    sleep() {
+        this.energy += 20;
+        console.log('Energy is increasing, currently at:', this.energy)
+    }
+    getColor() {
+        console.log(this.color)
+    }
+}
+
+class Cat extends Animal {
+    constructor(sound = 'purr', canJumpHigh = true, canClimbTrees = true, color, energy) {
+        super(color, energy);
+        this.sound = sound;
+        this.canClimbTrees = canClimbTrees;
+        this.canJumpHigh = canJumpHigh;
+    }
+    makeSound() {
+        console.log(this.sound);
+    }
+}
+
+class Bird extends Animal {
+    constructor(sound = 'chirp', canFly = true, color, energy) {
+        super(color, energy);
+        this.sound = sound;
+        this.canFly = canFly;
+    }
+    makeSound() {
+        console.log(this.sound);
+    }
+}
+
+class HouseCat extends Cat {
+    constructor(houseCatSound = "meow", sound,canJumpHigh,canClimbTrees, color,energy) {
+        super(sound,canJumpHigh,canClimbTrees, color,energy);
+        this.houseCatSound = houseCatSound;
+    }
+    makeSound(option) {
+        if (option) {
+            super.makeSound();
+        }
+        console.log(this.houseCatSound);
+    }
+}
+
+class Tiger extends Cat {
+    constructor(tigerSound = "Roar!", sound,canJumpHigh,canClimbTrees, color,energy) {
+        super(sound,canJumpHigh,canClimbTrees, color,energy);
+        this.tigerSound = tigerSound;
+    }
+    makeSound(option) {
+        if (option) {
+            super.makeSound();
+        }
+        console.log(this.tigerSound);
+    }
+}
+
+class Parrot extends Bird {
+    constructor(canTalk = false, sound,canFly, color,energy) {
+        super(sound,canFly, color,energy);
+        this.canTalk = canTalk;
+    }
+    makeSound(option) {
+        if (option) {
+            super.makeSound();
+        }
+        if (this.canTalk) {
+            console.log("I'm a talking parrot!");
+        }
+    }
+}
+
+var fiji = new Parrot(false); // we're passing `false` to the constructor so that fiji can't talk
+var polly = new Parrot(true); // we're passing `true` to the constructor so that polly can talk
+
+fiji.makeSound(); // undefined
+fiji.makeSound(true); // chirp
+
+polly.makeSound(); // I'm a talking parrot!
+polly.makeSound(true); // chirp, I'm a talking parrot!
+
+polly.color; // yellow
+polly.energy; // 100
+
+polly.isActive(); // Energy is decreasing, currently at: 80
+
+var penguin = new Bird("shriek", false, "black and white", 200); // setting all the custom properties
+penguin; // Bird {color: 'black and white', energy: 200, sound: 'shriek', canFly: false }
+
+penguin.sound; // 'shriek'
+penguin.canFly; // false
+penguin.color; // 'black and white'
+penguin.energy; // 200
+penguin.isActive(); // Energy is decreasing, currently at: 180
+
+var leo = new HouseCat();
+
+// leo, no purring please:
+leo.makeSound(false); // meow
+// leo, both purr and meow now:
+leo.makeSound(true); // purr, meow
+
+var cuddles = new Tiger();
+cuddles.makeSound(false); // Roar!
+cuddles.makeSound(true); // purr, Roar!
