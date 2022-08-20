af89b627f444941ad927e107ff2429811f882b7d
patricia.cosma <p.n.cosma@gmail.com>
2022-08-20 14:15:06 +0300

    typos correction

--- browser.js
+++ browser.js
@@ -38,6 +38,6 @@ let answer = prompt('What is your name?');
 if (typeof(answer) === 'string') {
     var h1 = document.createElement('h1')
     h1.innerText = answer;
-    document.body.innerText = ''; // de ce asta?
+    document.body.innerText = '';
     document.body.appendChild(h1);
 }
