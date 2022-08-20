0156edb743f911af85f87ea3fa4695c1f2fdf89f
patricia.cosma <p.n.cosma@gmail.com>
2022-08-20 14:09:29 +0300

    JS selectors & event handling

--- browser.js
+++ browser.js
@@ -1,7 +1,7 @@
 // DOM manipulation
 
 // // update the document object 
-const h2 = document.createElement('h2'); // passing the tag names as a JS spring & assign the statement to a const variable
+const h2 = document.createElement('h2'); // passing the tag names as a JS string & assign the statement to a const variable
 // it is still undefined as it needs to also be attached to the DOM structure
 // in order to see it after it is attached to the DOM, we also need some text, otherwise it will be blank & no visible change will be spotted
 h2.innerText = "This is a h2 heading";
@@ -12,3 +12,16 @@ h2.setAttribute ('class', 'secondary');
 // then, append the h2 to the DOM
 document.body.appendChild(h2);
 
+// JavaScript selectors
+document.querySelector('p') // selects the 1st paragraph in the web page
+document.querySelectorAll('p') // selects all paragraph within the page
+document.getElementById('heading') // element with that ID - if there is nothing with that ID, null will be returned
+document.getElementsByClassName('center') // elements with that class name - if there is no such class, empty collection with [] will be returned
+
+// event handling
+const target = document.querySelector('body') // the event will happen in the body
+function handleClick() {
+    console.log('clicked the body');
+}
+target.addEventListener('click', handleClick) // 2 paramters: event type as a string value and secondly, the function
+// when clicking anywhere in the body, the text from the function will be output in the console
