9aa002fa44cd121ad79071040b5ed0fb9e23946e
patricia.cosma <p.n.cosma@gmail.com>
2022-08-21 16:36:31 +0300

    input from HTML form and display text

--- browser.js
+++ browser.js
@@ -41,3 +41,16 @@ if (typeof(answer) === 'string') {
     document.body.innerText = '';
     document.body.appendChild(h1);
 }
+
+// take input from a HTML form and display the text from the user 
+var h1 = document.createElement('h1')
+h1.innerText = "Type into the input to make this text change"
+var input = document.createElement('input')
+input.setAttribute('type', 'text')
+document.body.innerText = '';
+document.body.appendChild(h1);
+document.body.appendChild(input);
+
+input.addEventListener('change', function() {
+    console.log(input.value)
+})
